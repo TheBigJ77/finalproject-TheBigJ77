c6cb03b3389636f2cec0cbeb069e151c8fed37fe
moraj0721 <moraj0721@HRH-IC0027319.wrdsb.ca>
2016-01-26 14:21:50 -0500

    unfininished

---
+++ src/Rotten.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author moraj0721
+ */
+class Rotten {
+
+    Rotten(int RandomX, int RandomY, int i, int i0) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
